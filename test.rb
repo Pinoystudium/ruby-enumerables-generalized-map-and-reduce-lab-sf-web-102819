591a286afec028cb158c8de40d28c930ee6688fd
Michael Espina <michael_espina@yahoo.com>
2019-10-28 14:03:57 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,10 +1,21 @@
-def make_sandwich(element1, element2)
-  base = "A #{element1} and #{element2}"
-  puts base
-  yield
-  #base
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length do
+    puts false if !source_array[i]
+    i += 1
+  end
+  puts true
 end
- 
-make_sandwich("chicken", "a sense of malaise") do |innards|
-  puts "making some tasty stuff..."
+
+#reduce_to_all_true([2,4,3])
+
+def reduce_to_any_true(source_array)
+  i = 0
+  while i < source_array.length do
+    puts true if source_array[i]
+    i += 1
+  end
+  puts false
 end
+
+#reduce_to_any_true([3,2,4])
